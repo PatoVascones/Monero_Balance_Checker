7494b794dc11465f8267eac15e8fc7765c7950eb
PatoVascones <40489834+PatoVascones@users.noreply.github.com>
2018-08-17 15:45:39 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Monero_Balance_Checker
+# Monero balance checker
+
+
+### Usage
+
+1. Clone repository
+2. `cd monerobalancechecker/`
+3. `bundle`
+4. `ruby check_monero.rb`
+
+### Result:
+```
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+[-] Gathering data ... Done!
+
+Your current Monero balance from moneropool.com is: 0.20227491362
+And your current hashrate is 497.00 H
+
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+```
+### Example:
+![](.gifs/monerochecker.gif)
---
+++ checker_monero.rb
@@ -0,0 +1,31 @@
+require 'httparty'
+require 'nokogiri'
+require 'tty-spinner'
+
+url = 'https://monerohash.com/api/stats_address?address=49Jt4tzbvZ5PyEMub6tNDGKP4zxogN9t1VACVWgTEcMwhtCGjxrDyt5XCDHG6XpA2U1uWsnsyKYdrL25Vp6y2pou2bdboCZ&longpoll=true'
+stats =
+puts "\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+puts "\n"
+
+#Creating spinner object.
+spinner = TTY::Spinner.new("[:spinner] Gathering data ...", format: :classic)
+#Looping until stats = true.
+spinner.run('Done!') do
+  stats = HTTParty.get(url)
+end
+#Setting variables.
+balance = (stats['stats']['balance'].to_f)/1000000000000
+hashrate=(stats['stats']['hashrate'])
+#Getting current price of Monero by parsing worldcoinindex website.
+price_stats = HTTParty.get('https://www.worldcoinindex.com/coin/monero')
+dom = Nokogiri::HTML(price_stats.body)
+price = dom.css('.coinprice')[0].text
+price = price.gsub(/\r\n?/, "").delete(' ').tr('$','').to_f
+pending_balance = price * balance
+#Printing data to console.
+puts "\nYour current Monero balance from monerohash.com is:\n
+#{balance}\n
+which equates to:\n
+$#{pending_balance} USD today."
+puts "\nAnd your current hashrate is #{hashrate}"
+puts "\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
